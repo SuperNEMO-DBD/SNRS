ec07381e7244e2e8345ca0db6040536402daa215
Francois Mauger <mauger@lpccaen.in2p3.fr>
2021-05-30 01:16:53 +0200

    Update scripts

--- build.bash
+++ build.bash
@@ -15,6 +15,9 @@ if [ -z ${BX_BAYEUX_INSTALL_DIR} ]; then
     bayeux_3_5_0_setup
 fi
 
+# Example:
+#  $ export RAW_LTD_DATA_DIR=/data/SuperNEMO/RealSources/SNLTD_3D_measurements
+
 rawLtdOpt=
 if [ -n "${RAW_LTD_DATA_DIR}" ]; then
    rawLtdOpt=-DRAW_LTD_DATA_DIR="${RAW_LTD_DATA_DIR}" 
@@ -23,6 +26,8 @@ fi
 cmake -DCMAKE_INSTALL_PREFIX=${install_dir} \
       -DBayeux_DIR=$(bxquery --cmakedir) \
       -DSNRS_DEVELOPER_BUILD=ON \
+      -DSNRS_GENERATE_DATA=ON \
+      -DSNRS_WITH_DOC=ON \
       ${rawLtdOpt} \
     ${src_dir}
 if [ $? -ne 0 ]; then
